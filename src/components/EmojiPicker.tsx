e062d23e1efc256eba82b5e6cce3ae82c0d6fc25
Nobeen <n.islam.1@outlook.com>
2022-03-08 16:03:39 +0000

    Added store array button and variable. Currently does add onto the array when you click the button, but is not automatic

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,39 +1,47 @@
 import { useState } from "react";
 
 export default function EmojiPicker(): JSX.Element {
-  const [[emojiValueFromCurrentRender,previousEmoji], queueRerenderWithNewEmojiValues] =
-    useState(["🍳",""]);
+  const [
+    [emojiValueFromCurrentRender, previousEmoji],
+    queueRerenderWithNewEmojiValues,
+  ] = useState(["🍳", ""]);
 
+  const [storedEmojiArray, rerenderStoredEmojiArray] = useState<string[]>([])
 
   const handleChangetoSmiley = () => {
-    queueRerenderWithNewEmojiValues(["😀",emojiValueFromCurrentRender]);
+    queueRerenderWithNewEmojiValues(["😀", emojiValueFromCurrentRender]);
   };
 
-  const handleChangetoCook= () => {
-    queueRerenderWithNewEmojiValues(["🍳",emojiValueFromCurrentRender]);
+  const handleChangetoCook = () => {
+    queueRerenderWithNewEmojiValues(["🍳", emojiValueFromCurrentRender]);
   };
 
-  const handleChangetoBeans= () => {
-    queueRerenderWithNewEmojiValues(["🌯",emojiValueFromCurrentRender]);
+  const handleChangetoBeans = () => {
+    queueRerenderWithNewEmojiValues(["🌯", emojiValueFromCurrentRender]);
   };
 
-  const handleChangetoMonkey= () => {
-    queueRerenderWithNewEmojiValues(["🐒",emojiValueFromCurrentRender]);
+  const handleChangetoMonkey = () => {
+    queueRerenderWithNewEmojiValues(["🐒", emojiValueFromCurrentRender]);
   };
 
+  const handleStoreEmojis = () => {
+    rerenderStoredEmojiArray([...storedEmojiArray,emojiValueFromCurrentRender])
+  }
 
-  console.log(emojiValueFromCurrentRender,previousEmoji);
+  console.log(emojiValueFromCurrentRender, previousEmoji, storedEmojiArray);
 
   return (
     <>
       <h1>Emoji picker</h1>
-      <p>Your previous emoji: {emojiValueFromCurrentRender}</p>
-      <p>Counter: {previousEmoji}</p>
+      <p>Stored Emojis: {storedEmojiArray}</p>
+      <p>Your previous emoji: {previousEmoji}</p>
+      <p>Counter: {emojiValueFromCurrentRender}</p>
       <button onClick={handleChangetoSmiley}>😀</button>
       <button onClick={handleChangetoCook}>🍳</button>
       <button onClick={handleChangetoBeans}>🌯</button>
       <button onClick={handleChangetoMonkey}>🐒</button>
       <hr />
+      <button onClick = {handleStoreEmojis}>Store Emojis</button>
     </>
   );
 }
